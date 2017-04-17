42c68b86b1ac64e6b31cf73592af8de002e04464
Jeff Shaw <jshaw@rocketfuel.com>
2017-04-17 17:45:47 -0400

    better handling for java/mvn/com/fasterxml/FasterxmlJacksonAll

--- src/main/resources/com/rocketfuel/build/db/mvn/module_paths.sql
+++ src/main/resources/com/rocketfuel/build/db/mvn/module_paths.sql
@@ -1,12 +1,12 @@
-CREATE VIEW mvn.module_paths AS
+CREATE OR REPLACE VIEW mvn.module_paths AS
 SELECT
   id,
   array_to_string(
       CASE WHEN path[1:3] = array['java', 'com', 'rocketfuel'] THEN path[4:array_length(path, 1)]
-           ELSE path
+      WHEN path[1:2] = array['java', 'mvn'] THEN array[path[2] || '/' || array_to_string(path[3:array_length(path, 1)], '.', NULL)]
+      ELSE path
       END,
       '/',
       NULL
   ) AS path
 FROM mool.blds
-WHERE array[path[1], path[2]] != array['java', 'mvn']
