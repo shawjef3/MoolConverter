b5bb8a5a197a6a47b86ab803d8d7402ba3132bf4
Jeff Shaw <jshaw@rocketfuel.com>
2017-04-28 15:54:13 -0400

    java.com.rocketfuel.modeling.athena.core.common.Common needs the remote grid.modeling dependency

--- src/main/resources/com/rocketfuel/build/db/hacks.sql
+++ src/main/resources/com/rocketfuel/build/db/hacks.sql
@@ -8,7 +8,16 @@ WHERE bld_to_sources.bld_id = blds.id
 --This fixes a wrong version of AdScoringInfo to be used during compiling.
 --This is a class conflict that should be resolved in vostok.
 DELETE FROM mool.bld_to_bld
-USING mool.blds
-WHERE bld_to_bld.target_id = blds.id
-  AND group_id = 'com.rocketfuel.grid.modeling'
-  AND artifact_id = 'grid.modeling';
+WHERE
+  target_id IN (
+    SELECT id
+    FROM mool.blds
+    WHERE group_id = 'com.rocketfuel.grid.modeling'
+          AND artifact_id = 'grid.modeling'
+          AND version = '103.32.0'
+  )
+  AND source_id NOT IN (
+    SELECT id
+    FROM mool.blds
+    WHERE path = array['java', 'com', 'rocketfuel', 'modeling', 'athena', 'core', 'common', 'Common']
+  )
