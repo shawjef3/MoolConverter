c7c8806760cfcec7ca2c46d1893646feded4b3e4
Radim Kubacki <rkubacki@rocketfuel.com>
2017-07-01 18:28:55 +0200

    Try project names based on lib names and cut lenght
    If project name created from RELCFG artifact is too long try to build it
    from used libraries.

--- src/main/resources/com/rocketfuel/build/db/gradle/project_mapping.sql
+++ src/main/resources/com/rocketfuel/build/db/gradle/project_mapping.sql
@@ -1,5 +1,5 @@
 CREATE OR REPLACE VIEW gradle.project_mapping AS (
-	SELECT bld_id, bld_path, string_agg(DISTINCT array_to_string(path, '-'), ',') AS prj_path
+	SELECT bld_id, array_to_string(bld_path, '-') AS bld_path, string_agg(DISTINCT array_to_string(path, '-'), ',') AS prj_path
 	FROM gradle.build_tree
-	GROUP BY bld_id, bld_path
+	GROUP BY bld_id, array_to_string(bld_path, '-')
 )
--- src/main/scala/com/rocketfuel/build/db/gradle/ProjectMapping.scala
+++ src/main/scala/com/rocketfuel/build/db/gradle/ProjectMapping.scala
@@ -1,11 +1,12 @@
 package com.rocketfuel.build.db.gradle
 
+import com.rocketfuel.build.Logger
 import com.rocketfuel.build.db.Deployable
 import com.rocketfuel.sdbc.PostgreSql._
 
 case class ProjectMapping(prj_path: String, bld_id: Int, bld_path: String)
 
-object ProjectMapping extends Deployable {
+object ProjectMapping extends Deployable with Logger {
   val list = Select[ProjectMapping]("SELECT prj_path, bld_id, bld_path FROM gradle.project_mapping")
 
   val deployQuery = Ignore.readClassResource(classOf[ProjectMapping], "project_mapping.sql")
@@ -16,7 +17,12 @@ object ProjectMapping extends Deployable {
   override def undeploy()(implicit connection: Connection): Unit =
     Ignore.ignore("DROP VIEW IF EXISTS gradle.project_mapping CASCADE")
 
-  def normalizeProjectName(prjName: String): String = {
+  def normalizeProjectName(prjName: String, bldPaths: Vector[String] = Vector.empty): String = {
+    val normalized = normalizeProjectNameImpl(prjName, bldPaths)
+    if (normalized.length > 50) normalized.substring(0, 50) else normalized
+  }
+
+  private def normalizeProjectNameImpl(prjName: String, bldPaths: Vector[String] = Vector.empty): String = {
     def longestCommonPrefix(list: String*) = list.foldLeft("")((_, _) =>
       (list.min.view, list.max.view).zipped.takeWhile(v => v._1 == v._2).unzip._1.mkString)
 
@@ -26,20 +32,45 @@ object ProjectMapping extends Deployable {
     else {
       val normalPrjNames = prjNames.map { normalizeSingleProjectName(_) }
       val prefix = longestCommonPrefix(normalPrjNames: _*)
-      prefix + normalPrjNames.map { _.substring(prefix.length)}.mkString(",")
+      val shorterPrjName = prefix + normalPrjNames.map { _.substring(prefix.length)}.mkString(",")
+
+      if (shorterPrjName.length > 100) {
+        logger.warn(s"project name too long $shorterPrjName")
+        val fromBldPaths = bldPaths.mkString(",")
+        val normalFromBldPaths = normalizeProjectName(fromBldPaths)
+        if (!normalFromBldPaths.isEmpty && normalFromBldPaths.length < shorterPrjName.length)
+          "lib-" + normalFromBldPaths else shorterPrjName
+      }
+      else
+        shorterPrjName
     }
   }
 
   private def normalizeSingleProjectName(prjName: String): String = {
-    val prjNameSegments = prjName.replaceAll("java-com-rocketfuel", "j-c-r").split('-').toVector
+    val prjNameSegments = prjName
+        .replaceAll("-analyze_coefficients", "")
+        .replaceAll("grid.epiphany.jobs.", "")
+        .replaceAll("grid.epiphany.", "")
+        .replaceAll("perfdiagnozer_", "")
+        .replaceAll("-retargeting", "-rt")
+        .replaceAll("java-com-rocketfuel-modeling-athena-pipelines-conversion", "j-c-r-m-a-p-c")
+        .replaceAll("java-com-rocketfuel-modeling-athena-pipelines", "j-c-r-m-a-p")
+        .replaceAll("java-com-rocketfuel-modeling-athena", "j-c-r-m-a")
+        .replaceAll("java-com-rocketfuel-common-message-protobuf", "protobuf")
+      .replaceAll("java-com-rocketfuel", "j-c-r").split('-').toVector
     val shortPrjName = prjNameSegments.dropRight(1).mkString("-")
 
-    // TODO handle path with ','
-    if (shortPrjName.endsWith(prjNameSegments(prjNameSegments.size - 1).replace('.', '-')))
+    if (shortPrjName.endsWith(prjNameSegments(prjNameSegments.size - 1).replace('.', '-')) ||
+      prjNameSegments(prjNameSegments.size - 1).matches("[a-zA-Z_]+_pipeline"))
       shortPrjName else prjNameSegments.mkString("-")
   }
 
-  def projectNames()(implicit connection: Connection): collection.SortedSet[String] = collection.SortedSet(
-      list.vector().map { pm => normalizeProjectName(pm.prj_path) }: _*
-  )
+  def projectNames()(implicit connection: Connection): collection.SortedSet[String] = {
+    val prjBldMap: Map[String, Vector[String]] = list.vector().filter { pm => !pm.bld_path.startsWith("java-mvn")
+    }.foldLeft(Map.empty[String, Vector[String]]) { (map, pm) =>
+      map + (pm.prj_path -> (map.getOrElse(pm.prj_path, Vector.empty) :+ pm.bld_path))
+    }
+    val prjNames = prjBldMap.map { case (k, v) => normalizeProjectName(k, v) }.toSeq
+    collection.SortedSet(prjNames: _*)
+  }
 }
